f1cee92f143493591733b1c1ac49ad214bc99945
diegofernandes.47 <diegof@47.github.com>
2024-10-20 13:41:59 -0300

    Segundo commit

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 function mostrarCarros(event) {
-    event.preventDefault();  // Isso evita que a página seja recarregada
+    event.preventDefault();  
 
-    // Obtendo os valores dos campos
+    
     let cartao = document.getElementById('cartao').value;
     let validade = document.getElementById('validade').value;
     let codigo = document.getElementById('codigo').value;
 
-    // Verificando se os campos estão preenchidos (só por segurança)
+    
     if (cartao && validade && codigo) {
-        // Dados de carros para exibir
+      
         let carros = [
             { nome: 'Você é o Relâmpago McQueen', imagem: 'img/mcQueen.jpeg' },
             { nome: 'Você é o Mate', imagem: 'img/mate.jpeg' },
@@ -18,10 +18,10 @@ function mostrarCarros(event) {
             { nome: 'Você é o Chick Hicks', imagem: 'img/chick_hicks.jpeg' }
         ];
 
-        // Escolher um carro aleatoriamente
+       
         let carroEscolhido = carros[Math.floor(Math.random() * carros.length)];
 
-        // Criando novos elementos para o resultado
+       
         let divResultado = document.createElement('div');
         divResultado.classList.add('resultado');
 
@@ -32,11 +32,11 @@ function mostrarCarros(event) {
         imagemCarro.src = carroEscolhido.imagem;
         imagemCarro.alt = carroEscolhido.nome;
 
-        // Adicionando os elementos à nova div
+     
         divResultado.appendChild(nomeCarro);
         divResultado.appendChild(imagemCarro);
 
-        // Adicionando o resultado à div de resultados
+     
         document.getElementById('resultados').appendChild(divResultado);
     } else {
         alert('Por favor, preencha todos os campos!');
